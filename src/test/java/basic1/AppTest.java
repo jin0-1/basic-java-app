a574ad1136e42f26ad3212965cf5461702f43ea1
jin0-1 <164625254+jin0-1@users.noreply.github.com>
2025-11-02 11:52:35 +0530

    Update AppTest.java

--- src/test/java/basic1/AppTest.java
+++ src/test/java/basic1/AppTest.java
@@ -5,34 +5,35 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for App class.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase 
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
-     * Rigourous Test :-)
+     * Test if App.main runs without error
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testAppMainRuns() {
+        try {
+            App.main(new String[] {});
+            assertTrue(true);  // If no exception, test passes
+        } catch (Exception ex) {
+            fail("App.main threw an exception: " + ex.getMessage());
+        }
     }
 }
